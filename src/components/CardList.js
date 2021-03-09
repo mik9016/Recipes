dadacb5b7f1f6e07af5d185971a433aa5004e4a1
mik9016 <mikolajgruszecki@o2.pl>
2021-03-09 22:45:36 +0100

    add list rendering in CardList

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,24 +1,44 @@
 import React from "react";
 import CardRecipe from "./CardRecipe";
-import Img from '../assets/Cheescake.jpg';
+import Img from "../assets/Cheescake.jpg";
 
 export default function CardList(props) {
   const img = Img;
 
-  const hardcodedCard = {
-    image: img,
-    title: "Cheescake",
-    description: "Extraordinary Cheescake to Lorem Impus Dolores ",
-  };
+  const hardcodedArr = [
+    {
+      id: 1,
+      image: img,
+      title: "Cheescake",
+      description: "Extraordinary Cheescake to Lorem Impus Dolores 1",
+    },
+    {
+      id: 2,
+      image: img,
+      title: "Cheescake",
+      description: "Extraordinary Cheescake to Lorem Impus Dolores2 ",
+    },{
+      id: 3,
+      image: img,
+      title: "Cheescake",
+      description: "Extraordinary Cheescake to Lorem Impus Dolores3 ",
+    }
+  ];
 
   return (
     <div>
       <h1>Card List</h1>
-      <CardRecipe
-        image= {hardcodedCard.image}
-        title={hardcodedCard.title}
-        description={hardcodedCard.description}
-      />
+      {hardcodedArr.map((card) => {
+        console.log();
+        return (
+          <CardRecipe
+            key={card.id}
+            image={card.image}
+            title={card.title}
+            description={card.description}
+          />
+        );
+      })}
     </div>
   );
 }
